84952c369c4db7e1c96491f38bbb3aa71a72e627
janga_politex <zyzzrasakama@gmail.com>
2023-02-06 20:18:43 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            string? konus = Console.ReadLine();
+            Console.WriteLine("Введите первое число");
+            string? sinus = Console.ReadLine();
+            Console.WriteLine("Введите первое число");
+            string? pinus = Console.ReadLine();
+            Console.WriteLine("Введите первое число");
+            string? pahannus = Console.ReadLine();
+            Console.WriteLine($"{konus}");
+            Console.WriteLine($"{sinus}");
+            Console.WriteLine($"{pinus}");
+            Console.WriteLine($"{pahannus}");
+        }
+    }
+}
