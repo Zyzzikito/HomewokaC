fd300a82855863c5d3dace31b14f455448018c19
B4bro4ka <98108675+Zyzzikito@users.noreply.github.com>
2023-02-13 16:03:08 +0300

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,28 +4,6 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите первое число");
-            string? konus = Console.ReadLine();
-            Console.WriteLine("Введите первое число");
-            string? sinus = Console.ReadLine();
-            Console.WriteLine("Введите первое число");
-            string? pinus = Console.ReadLine();
-            Console.WriteLine("Введите первое число");
-            string? pahannus = Console.ReadLine();
-            Console.WriteLine($"{konus}");
-            Console.WriteLine($"{sinus}");
-            Console.WriteLine($"{pinus}");
-            Console.WriteLine($"{pahannus}");
-        }
-    }
-}
-
-﻿namespace ConsoleApp3
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
             int a = 0;
             int b = 0;
             int c = 0;
@@ -44,6 +22,16 @@
             Console.WriteLine($"{sinus}");
             Console.WriteLine($"{pinus}");
             Console.WriteLine($"{pahannus}");
+            }
+            catch
+            {
+                Console.WriteLine("Вы ввели не число");
+                return;
+            }
+            Console.WriteLine($"{a}");
+            Console.WriteLine($"{b}");
+            Console.WriteLine($"{c}");
+            Console.WriteLine($"{d}");
         }
     }
 }
