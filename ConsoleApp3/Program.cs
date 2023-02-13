ea6278df253a233a299421a4614973ce23410ad7
B4bro4ka <98108675+Zyzzikito@users.noreply.github.com>
2023-02-13 16:00:16 +0300

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -19,3 +19,31 @@
         }
     }
 }
+
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            int d = 0;
+            try
+            {
+            Console.WriteLine("Введите первое число");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите третье число");
+            c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите четвертое число");
+            d = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{konus}");
+            Console.WriteLine($"{sinus}");
+            Console.WriteLine($"{pinus}");
+            Console.WriteLine($"{pahannus}");
+        }
+    }
+}
